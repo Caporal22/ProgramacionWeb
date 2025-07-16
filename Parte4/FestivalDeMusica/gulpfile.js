f07283c0d042de8b1b2a4ade083848abeb698c27
DanieliniCaporal <dasacaporal@gmail.com>
2025-07-15 23:40:09 -0600

    Agregando funciones al gulp y construcción del header en SASS

--- Parte4/FestivalDeMusica/gulpfile.js
+++ Parte4/FestivalDeMusica/gulpfile.js
@@ -1,4 +1,4 @@
-import { src, dest } from "gulp";
+import { src, dest, watch } from "gulp";
 // Importar el paquete de SASS
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
@@ -7,9 +7,20 @@ const sass = gulpSass(dartSass); // Compilar SASS a CSS
 
 export function css(done) {
   src("src/scss/app.scss") // Ruta del archivo SASS
-    .pipe(sass()) // Compilar SASS a CSS
+    .pipe(sass().on("error", sass.logError)) // Compilar SASS a CSS
     .pipe(dest("build/css")); // Ruta de destino del CSS compilado
 
   done();
   done();
+}
+
+export function watchFiles() {
+  // Observa los cambios en los archivos SASS y ejecuta la tarea css
+  src("src/scss/**/*.scss").on("change", css);
+}
+
+export function dev() {
+  // Inicia el observador de archivos
+  watch("src/scss/**/*.scss", css);
+  // watch(js);
 }
